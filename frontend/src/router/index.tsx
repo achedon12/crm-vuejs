e37f624ff0dc10d6b1499760b25b6dd1788a219c
Achedon12 <leoderoinleo@gmail.com>
2025-07-11 18:34:30 +0200

    feat: appLayout

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -3,9 +3,9 @@ import Dashboard from "@/pages/admin/Dashboard.vue";
 import Login from "@/pages/auth/Login.vue";
 import AuthLayout from "@/layouts/AuthLayout.vue";
 import {useAuthStore} from "@/stores/authStore";
-import AppLAyout from "../layouts/AppLAyout.vue";
-import Preferences from "../pages/preferences/Preferences.vue";
-import Settings from "../pages/settings/Settings.vue";
+import AppLayout from "@/layouts/AppLayout.vue";
+import Preferences from "@/pages/preferences/Preferences.vue";
+import Settings from "@/pages/settings/Settings.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -36,7 +36,7 @@ const router = createRouter({
             name: 'admin',
             path: '/admin',
             redirect: {name: 'dashboard'},
-            component: AppLAyout,
+            component: AppLayout,
             beforeEnter: (to, from, next) => {
                 const authStore = useAuthStore()
                 if (!authStore.user || !authStore.token) {
