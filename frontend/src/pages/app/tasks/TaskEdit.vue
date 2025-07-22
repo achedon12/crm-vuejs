7511297664dc6f22ce336bab6533a28c7d7c7ef7
Achedon12 <leoderoinleo@gmail.com>
2025-07-22 10:42:40 +0200

    fix: edit and display tasks

--- frontend/src/pages/app/tasks/TaskEdit.vue
+++ frontend/src/pages/app/tasks/TaskEdit.vue
@@ -433,23 +433,12 @@ const handleSendComment = async () => {
                         }}</p>
                       <p class="text-xs text-gray-500">{{ new Date(comment.createdAt).toLocaleString() }}</p>
                     </div>
-                    <button
-                        v-if="isRealmAdministrator || comment.user?._id === authStore.user._id"
-                        class="btn btn-ghost btn-xs"
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
-                           stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
-                      </svg>
-                    </button>
                   </div>
                   <p class="mt-2 text-sm">{{ comment.content }}</p>
                 </div>
               </div>
             </div>
 
-            <!-- Comment Form -->
             <div class="mt-6">
               <div class="flex items-start gap-4">
                 <UserAvatar size="small"/>
